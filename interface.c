24ef589cdb526360401f3ba342e94962d62cf1cf
causehhc <hhc404404@gamil.com>
2021-09-09 19:26:20 +0800

    add staticLib

--- interface.c
+++ interface.c
@@ -3,17 +3,17 @@
 //
 
 #include "interface.h"
-#include "real_driver.h"
-#include "venv_driver.h"
+
+unsigned char VENV_getIr(unsigned char num);
+void VENV_go(unsigned char relD);
+void VENV_display_mazeInfo();
 
 unsigned char get_ir(unsigned char num){
   return VENV_getIr(num);
-//  return HARD_getIr(num);
 }
 
 void go_rel(unsigned char relD){
   VENV_go(relD);
-//  HARD_go(relD);
 }
 
 void display_mazeInfo(){
--- main.c
+++ main.c
@@ -16,7 +16,37 @@ void interface_test(){
   }
 }
 
+unsigned char abs_to_rel(unsigned char absD, unsigned char absD_t){
+  //absD：当前绝对方向
+  //absD_t：期望转换到哪个绝对方向
+  unsigned char relD = (absD_t - absD) % 4;
+  if(relD > 127)	relD += 4;
+  //返回相对方向
+  return relD;
+}
+
+void test(){
+  setbuf(stdout, 0);
+  int count=0;
+  int now_dir = 1;
+  while(count++<100){
+    display_mazeInfo();
+    printf("front:%d\n", get_ir(3));
+    printf("right:%d\n", get_ir(5));
+    printf("left:%d\n", get_ir(1));
+
+    fflush(stdin);
+    int abs_dir = getc(stdin)-'0';
+    if(abs_dir!=now_dir){
+      go_rel(abs_to_rel(now_dir, abs_dir));
+    }
+    go_rel(0);
+    now_dir = abs_dir;
+  }
+}
+
 int main() {
+  test();
   interface_test();
   return 0;
 }
